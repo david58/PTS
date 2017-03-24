73154bf60acd470d2c0165984614b5fbad9ce854
Dávid <davidb@ksp.sk>
2017-03-24 01:02:01 +0100

    zapracované prvé pripomienky

--- program.py
+++ program.py
@@ -1,70 +1,82 @@
-import getpass
 import hashlib
 
 
 # vyziada a overi heslo
 def autentifikuj():
     hash_hesla = open("heslo.txt").read().strip()
-    #REVIEW: input vie brat jeden argument a to je vyzva pred vstupom, pouzit radsej namiesto print...input
-    print("Zadaj heslo!")
-    heslo = input()
+    heslo = input("Zadaj heslo!\n")
     # neda sa jednoducho testovat
     # heslo = getpass.getpass(prompt='Heslo: ', stream=None)
     if(hashlib.sha224(heslo.encode()).hexdigest() == hash_hesla):
-    	#REVIEW: return nie je funkcia, ziadne zatvorky netreba a je to neidiomaticke
-        return(True)
+        return True
     else:
         print("Nesprávne heslo")
-        #REVIEW: opat, return nepotrebuje zatvorky
-        return(False)
+        return False
 
 
+def points():
+    if(autentifikuj()):
+        if prikaz[1] in hraci:
+            hraci[prikaz[1]]["body"] += int(prikaz[2])
+        else:
+            hraci[prikaz[1]] = {}
+            hraci[prikaz[1]]["junior"] = False
+            hraci[prikaz[1]]["body"] = int(prikaz[2])
+            
+
+def reduction():
+    if(autentifikuj()):
+        for hrac in hraci:
+            percent = 1 - int(prikaz[1]) / 100
+            hraci[hrac]["body"] = int(hraci[hrac]["body"] * percent)
+            
+
+def junior():
+    if(autentifikuj()):
+        if prikaz[1] in hraci:
+            hraci[prikaz[1]]["junior"] = True
+        else:
+            hraci[prikaz[1]] = {}
+            hraci[prikaz[1]]["body"] = 0
+            hraci[prikaz[1]]["junior"] = True
+
+
+def ranking():
+    if(len(prikaz) > 1 and prikaz[1] == "junior"):
+        for hrac in sorted(hraci.items(),
+                           key=lambda x: x[1]["body"],
+                           reverse=True):
+            if(hrac[1]["junior"]):
+                print(hrac[0], hrac[1]["body"])
+    else:
+        for hrac in sorted(hraci.items(),
+                           key=lambda x: x[1]["body"],
+                           reverse=True):
+            print(hrac[0], hrac[1]["body"])
+            
+
+def myquit():
+    if(autentifikuj()):
+        quit()
 # zoznam hracov
 hraci = {}
 
-#REVIEW: program si ma na zaciatku vypytat nove heslo od uzivatela
+# REVIEW: program si ma na zaciatku vypytat nove heslo od uzivatela
+# ANSWER: ktorá veta v zadaní to hovorí?
 if(autentifikuj()):
-    # hash hesla
-    print(open("zadanie.txt").read()) #REVIEW: preco vypisat na zaciatku zadanie? to staci v repozitari
     while(True):
         prikaz = input().split()
-        #REVIEW: skarede a neprehladne, rozdelit do viacerych funkcii
+        # REVIEW: skarede a neprehladne, rozdelit do viacerych funkcii
         # prazdny riadok
         if(len(prikaz) == 0):
             continue
         elif(prikaz[0] == "points"):
-            if(autentifikuj()):
-                if prikaz[1] in hraci:
-                    hraci[prikaz[1]]["body"] += int(prikaz[2])
-                else:
-                    hraci[prikaz[1]] = {}
-                    hraci[prikaz[1]]["junior"] = False
-                    hraci[prikaz[1]]["body"] = int(prikaz[2])
+            points()
         elif(prikaz[0] == "reduce"):
-            if(autentifikuj()):
-                for hrac in hraci:
-                    percent = 1 - int(prikaz[1]) / 100
-                    hraci[hrac]["body"] = int(hraci[hrac]["body"] * percent)
+            reduction()
         elif(prikaz[0] == "junior"):
-            if(autentifikuj()):
-                if prikaz[1] in hraci:
-                    hraci[prikaz[1]]["junior"] = True
-                else:
-                    hraci[prikaz[1]] = {}
-                    hraci[prikaz[1]]["body"] = 0
-                    hraci[prikaz[1]]["junior"] = True
+            junior()
         elif(prikaz[0] == "ranking"):
-            if(len(prikaz) > 1 and prikaz[1] == "junior"):
-                for hrac in sorted(hraci.items(),
-                                   key=lambda x: x[1]["body"],
-                                   reverse=True):
-                    if(hrac[1]["junior"]):
-                        print(hrac[0], hrac[1]["body"])
-            else:
-                for hrac in sorted(hraci.items(),
-                                   key=lambda x: x[1]["body"],
-                                   reverse=True):
-                    print(hrac[0], hrac[1]["body"])
+            ranking()
         elif(prikaz[0] == "quit"):
-            if(autentifikuj()):
-                quit()
+            myquit()
