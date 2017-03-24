c5eb306924d940b53f74c814344b8c2569eae04a
Dávid <davidb@ksp.sk>
2017-03-24 01:50:12 +0100

    fixnuté ďaľšie pripomienky

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 import hashlib
+import sys
 
 
 # vyziada a overi heslo
@@ -7,95 +8,77 @@ def autentifikuj():
     heslo = input("Zadaj heslo!\n")
     # neda sa jednoducho testovat
     # heslo = getpass.getpass(prompt='Heslo: ', stream=None)
-    #REVIEW: if nepotrebuje zatvorky a je to neidiomaticke
-    if(hashlib.sha224(heslo.encode()).hexdigest() == hash_hesla):
+    if hashlib.sha224(heslo.encode()).hexdigest() == hash_hesla:
         return True
     else:
         print("Nesprávne heslo")
         return False
 
 
-def points():
-    #REVIEW: globalna premenna prikaz, vid ranking
-    #REVIEW: if nepotrebuje zatvorky a je to zle
-    if(autentifikuj()):
-        if prikaz[1] in hraci:
-            hraci[prikaz[1]]["body"] += int(prikaz[2])
+def points(hrac, body):
+    if autentifikuj():
+        if hrac in hraci:
+            hraci[hrac]["body"] += body
         else:
-            hraci[prikaz[1]] = {}
-            hraci[prikaz[1]]["junior"] = False
-            hraci[prikaz[1]]["body"] = int(prikaz[2])
+            hraci[hrac] = {}
+            hraci[hrac]["junior"] = False
+            hraci[hrac]["body"] = body
             
 
-def reduction():
-    #REVIEW: globalna premenna prikaz, vid ranking
-    if(autentifikuj()):
+def reduction(percenta):
+    if autentifikuj():
         for hrac in hraci:
-            percent = 1 - int(prikaz[1]) / 100
+            percent = 1 - percenta / 100
             hraci[hrac]["body"] = int(hraci[hrac]["body"] * percent)
             
 
-def junior():
-    #REVIEW: globalna premenna prikaz, vid ranking
-    if(autentifikuj()):
-        if prikaz[1] in hraci:
-            hraci[prikaz[1]]["junior"] = True
+def junior(hrac):
+    if autentifikuj():
+        if hrac in hraci:
+            hraci[hrac]["junior"] = True
         else:
-            hraci[prikaz[1]] = {}
-            hraci[prikaz[1]]["body"] = 0
-            hraci[prikaz[1]]["junior"] = True
+            hraci[hrac] = {}
+            hraci[hrac]["body"] = 0
+            hraci[hrac]["junior"] = True
 
 
-def ranking():
-    #REVIEW: tu sa dana podiferne vytvorena globalna premenna pouziva
-    #        bez spustenia som myslel, ze to crashne
-    #        tato funkcia nie je pouzitelna, pokial sa dopredu nenastavi globalna
-    #        premenna prikaz, velmi zle
-    #        premennu prikaz nie je dobre dat ani ako parameter priamo
-    #        spravny sposob je sparsovat ju v hlavnom cykle a potom dat funkcii ranking
-    #        napriklad boolovsky parameter junior
-    if(len(prikaz) > 1 and prikaz[1] == "junior"):
-        for hrac in sorted(hraci.items(),
-                           key=lambda x: x[1]["body"],
-                           reverse=True):
-            if(hrac[1]["junior"]):
-                print(hrac[0], hrac[1]["body"])
-    else:
-        for hrac in sorted(hraci.items(),
-                           key=lambda x: x[1]["body"],
-                           reverse=True):
+def ranking(vsetci):
+    if vsetci:
+        for hrac in sorted(hraci.items(), key=lambda x: x[1]["body"], reverse=True):
             print(hrac[0], hrac[1]["body"])
-            
+    else:
+        for hrac in sorted(hraci.items(), key=lambda x: x[1]["body"], reverse=True):
+            if hrac[1]["junior"]:
+                print(hrac[0], hrac[1]["body"])
+
 
 def myquit():
-    if(autentifikuj()):
-        quit()
+    if autentifikuj():
+        sys.exit()
 # zoznam hracov
 hraci = {}
 
 # REVIEW: program si ma na zaciatku vypytat nove heslo od uzivatela
 # ANSWER: ktorá veta v zadaní to hovorí?
 
-#REVIEW: nejaky kod by sa mal v pythone spustat len ak plati
-#        if __name__ == "__main__":
-#        cize ak tento subor neimportujeme ale spustame
-if(autentifikuj()):
-    #REVIEW: while nepotrebuje zatvorky a je to zle
-    while(True):
-        #REVIEW: velmi pofiderne vytvorena globalna premenna
-        #        vobec nie je na prvy pohlad jasne, preco toto funguje
-        prikaz = input().split()
-        # prazdny riadok
-        #REVIEW: if nepotrebuje zatvorky a je to neidiomaticke
-        if(len(prikaz) == 0):
-            continue
-        elif(prikaz[0] == "points"):
-            points()
-        elif(prikaz[0] == "reduce"):
-            reduction()
-        elif(prikaz[0] == "junior"):
-            junior()
-        elif(prikaz[0] == "ranking"):
-            ranking()
-        elif(prikaz[0] == "quit"):
-            myquit()
+if __name__ == "__main__":
+    if autentifikuj():
+        while True:
+            prikaz = input().split()
+            # prazdny riadok
+            if len(prikaz) == 0:
+                continue
+            elif prikaz[0] == "points":
+                points(prikaz[1], int(prikaz[2]))
+            elif prikaz[0] == "reduce":
+                reduction(int(prikaz[1]))
+            elif prikaz[0] == "junior":
+                junior(prikaz[1])
+            elif prikaz[0] == "ranking":
+                if len(prikaz) > 1 and prikaz[1] == "junior":
+                    vsetci = False
+                else:
+                    vsetci = True
+                ranking(vsetci)
+            elif prikaz[0] == "quit":
+                myquit()
