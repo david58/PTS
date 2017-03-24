241be024430b8fd7a4b095cce18948b5f28204fe
Dávid <davidb@ksp.sk>
2017-03-24 02:09:12 +0100

    fix PEP 257 -- Docstring

--- program.py
+++ program.py
@@ -1,10 +1,9 @@
 import hashlib
 import sys
 
-#REVIEW: chyba dokumentacia cez docstringy
 
-# vyziada a overi heslo
 def autentifikuj():
+    """vyziada a overi heslo"""
     hash_hesla = open("heslo.txt").read().strip()
     heslo = input("Zadaj heslo!\n")
     # neda sa jednoducho testovat
@@ -16,7 +15,8 @@ def autentifikuj():
         return False
 
 
-def points(hrac, body):
+def points(hraci, hrac, body):
+    """prida hracovi body"""
     if autentifikuj():
         if hrac in hraci:
             hraci[hrac]["body"] += body
@@ -24,16 +24,18 @@ def points(hrac, body):
             hraci[hrac] = {}
             hraci[hrac]["junior"] = False
             hraci[hrac]["body"] = body
-            
 
-def reduction(percenta):
+
+def reduction(hraci, percenta):
+    """každému zníži počet bodov o percentá"""
     if autentifikuj():
         for hrac in hraci:
             percent = 1 - percenta / 100
             hraci[hrac]["body"] = int(hraci[hrac]["body"] * percent)
-            
 
-def junior(hrac):
+
+def junior(hraci, hrac):
+    """označí hráča za juniora"""
     if autentifikuj():
         if hrac in hraci:
             hraci[hrac]["junior"] = True
@@ -43,7 +45,8 @@ def junior(hrac):
             hraci[hrac]["junior"] = True
 
 
-def ranking(vsetci):
+def ranking(hraci, vsetci):
+    """vypíše poradie hráčov"""
     if vsetci:
         for hrac in sorted(hraci.items(), key=lambda x: x[1]["body"], reverse=True):
             print(hrac[0], hrac[1]["body"])
@@ -54,17 +57,13 @@ def ranking(vsetci):
 
 
 def myquit():
+    """ukončí program"""
     if autentifikuj():
         sys.exit()
 
-#REVIEW: hraci tiez nemusia byt globalna premenna, radsej parameter do funkcii
-# zoznam hracov
-hraci = {}
-
-# REVIEW: program si ma na zaciatku vypytat nove heslo od uzivatela
-# ANSWER: ktorá veta v zadaní to hovorí?
 
 if __name__ == "__main__":
+    hraci = {}
     if autentifikuj():
         while True:
             prikaz = input().split()
@@ -72,16 +71,16 @@ if __name__ == "__main__":
             if len(prikaz) == 0:
                 continue
             elif prikaz[0] == "points":
-                points(prikaz[1], int(prikaz[2]))
+                points(hraci, prikaz[1], int(prikaz[2]))
             elif prikaz[0] == "reduce":
-                reduction(int(prikaz[1]))
+                reduction(hraci, int(prikaz[1]))
             elif prikaz[0] == "junior":
-                junior(prikaz[1])
+                junior(hraci, prikaz[1])
             elif prikaz[0] == "ranking":
                 if len(prikaz) > 1 and prikaz[1] == "junior":
                     vsetci = False
                 else:
                     vsetci = True
-                ranking(vsetci)
+                ranking(hraci, vsetci)
             elif prikaz[0] == "quit":
                 myquit()
